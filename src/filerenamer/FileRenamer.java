9fb9bb8735024bdbcbb3aedea6e5c34b8314d204
Tobias Savinainen <tobias@savinainen.se>
2014-06-10 23:40:33 +0200

    Commit

--- src/filerenamer/FileRenamer.java
+++ src/filerenamer/FileRenamer.java
@@ -1,27 +1,15 @@
 package filerenamer;
 
-import domain.Rule;
-import domain.RuleManager;
 import domain.exceptions.RuleExistException;
-import domain.rules.RuleRegex;
 import gui.MainFrame;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import technical.FileManager;
 
 public class FileRenamer {
 
     public static void main(String[] args) throws RuleExistException, FileNotFoundException, ClassNotFoundException {
-        
-//        String d = "hej på dig";
-//        String e = d.replaceAll("\\?", "_");
-//        
-//        System.out.println(e);
-        
-        
         setLookAndFeel();
 
         java.awt.EventQueue.invokeLater(new Runnable() {
